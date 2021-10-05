8287f2f53d6d4639d6b636647dd0b19626fdf82a
kimjunyun7 <kimjunyun7@gmail.com>
2021-10-05 23:18:19 +0700

    Initial commit

---
+++ src/main/java/com/example/chatapp/Controller/Controller.java
@@ -0,0 +1,13 @@
+package com.example.chatapp.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+}
