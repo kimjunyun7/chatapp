5475b0a2e9fa8ee7594b1c1d580d482102c69437
kimjunyun7 <kimjunyun7@gmail.com>
2021-10-13 01:22:26 +0700

    add roomList, addRoom

--- src/main/java/com/example/chatapp/Controller/ChatController.java
+++ src/main/java/com/example/chatapp/Controller/ChatController.java
@@ -1,16 +1,44 @@
 package com.example.chatapp.Controller;
 
+import com.example.chatapp.Model.ChatRoom;
+import com.example.chatapp.Model.ChatRoomForm;
+import com.example.chatapp.Repository.ChatRoomRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @Controller
+@RequiredArgsConstructor
 public class ChatController {
+    private final ChatRoomRepository chatRoomRepository;
 
     @GetMapping("/")
-    public String chat() {
-        return "chat";
+    public String rooms(Model model){
+        model.addAttribute("rooms",chatRoomRepository.findAllRoom());
+        return "roomList";
+    }
+
+    @GetMapping("/rooms/{id}")
+    public String room(@PathVariable String id, Model model){
+        ChatRoom room = chatRoomRepository.findRoomById(id);
+        model.addAttribute("room",room);
+        return "room";
+    }
+
+    @GetMapping("/new")
+    public String make(Model model){
+        ChatRoomForm form = new ChatRoomForm();
+        model.addAttribute("form",form);
+        return "newRoom";
+    }
+
+    @PostMapping("/room/new")
+    public String makeRoom(ChatRoomForm form){
+        chatRoomRepository.createChatRoom(form.getName());
+
+        return "redirect:/";
     }
 }
